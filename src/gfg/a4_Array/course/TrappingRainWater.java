2b8c7c82abc14a88832ed06991959c792e874fc1
gautyverma <gautyverma@gmail.com>
2022-08-25 14:37:06 +0530

    New

--- src/gfg/a4_Array/course/TrappingRainWater.java
+++ src/gfg/a4_Array/course/TrappingRainWater.java
@@ -4,38 +4,52 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class TrappingRainWater {
-	public static void main(String[] args) {
-		Integer[] arr = { 1,2,3};
-		rainWater(arr);
-	}
+  public static void main(String[] args) {
+    Integer[] arr = {3, 0, 5, 7, 2, 6};
+    System.out.println("Navie Solution - " + rainWaterNavie(arr));
+    System.out.println("Effective Solution - " + rainWaterEff(arr));
+  }
 
-	private static void rainWater(Integer[] arr) {
-		// TODO Auto-generated method stub
-		int res = 0;int sum=0;
-		int start = arr[0];
-		for(int i = 1;i<arr.length;i++) {
-			if(arr[i]>start) {
-				start=arr[i];
-				res+=sum;sum=0;
-			}
-			else {
-				sum+=start-arr[i];
-			}
-		}
-		System.out.println(res);
-		Collections.reverse(Arrays.asList(arr));
-		res = 0;sum=0;
-		start = arr[0];
-		for(int i = 1;i<arr.length;i++) {
-			if(arr[i]>start) {
-				start=arr[i];
-				res+=sum;sum=0;
-			}
-			else {
-				sum+=start-arr[i];
-			}
-		}
-		System.out.println(res);
-		
-	}
+  private static int rainWaterEff(Integer[] arr) {
+    int res = 0;
+    int n = arr.length;
+    int[] lmax = new int[n];
+    int[] rmax = new int[n];
+
+    lmax[0] = arr[0];
+    rmax[n-1] = arr[n-1];
+
+    for(int i = 1;i<n;i++){
+      lmax[i] = Math.max(arr[i],lmax[i-1]);
+    }
+    for(int i = n-2;i>=0;i--){
+      rmax[i] = Math.max(arr[i],rmax[i+1]);
+    }
+    for(int i = 0;i<n;i++){
+      res += Math.min(lmax[i],rmax[i]) - arr[i];
+    }
+  return res;
+  }
+
+  private static int rainWaterNavie(Integer[] arr) {
+    // TODO Auto-generated method stub
+    /*  Water cannot be stored on the corner bars so loop - 1 to n-1
+        for middle bars find the max value to left of it and right of it,
+        then take the min value among them.
+    */
+    int res = 0;
+    int n = arr.length; // size of array
+    for (int i = 1; i < n - 1; i++) {
+      int lmax = arr[i];
+      for (int j = 0; j < i; j++) {
+        lmax = Math.max(lmax, arr[j]);
+      }
+      int rmax = arr[i];
+      for (int j = i + 1; j < n; j++) {
+        rmax = Math.max(rmax, arr[j]);
+      }
+      res += Math.min(rmax, lmax) - arr[i];
+    }
+    return res;
+  }
 }
--- src/prac/Ridhi.java
+++ src/prac/Ridhi.java
@@ -1,10 +1,20 @@
 package prac;
 
 public class Ridhi {
-	public static void main(String[] args) {
-		int x = 1;
-		System.out.println(Integer.toBinaryString(x));
-		System.out.println(~x);
-		System.out.println(Integer.toBinaryString(~x));
+	public static void main(String []args){
+		System.out.println("Hello World");
+		int limit = 25;
+		int sum = 60;
+		double randomValue = 2.77;
+
+		for (int var = 1; var < limit; var = var + 3) {
+     System.out.println("var - "+var);
+			System.out.println("randomValue: " + randomValue);
+			for (int j = 1; j < limit; j = j + 2) {
+			System.out.println("j-"+j);
+				sum++;
+			}
+			System.out.println("Sum = " + sum);
+		}
 	}
 }
