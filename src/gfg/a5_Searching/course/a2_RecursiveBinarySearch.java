f0fa4c6cf041149dff860103e1c6362727c6e27d
Gauty <32440155+gautyverma@users.noreply.github.com>
2023-08-08 17:22:28 +0530

    DSA - searching - course - a2_RecursiveBinarySearch

--- src/gfg/a5_Searching/course/a2_RecursiveBinarySearch.java
+++ src/gfg/a5_Searching/course/a2_RecursiveBinarySearch.java
@@ -3,7 +3,7 @@ package gfg.a5_Searching.course;
 public class a2_RecursiveBinarySearch {
   public static void main(String[] args) {
     int[] arr = {10, 20, 30, 40, 50, 60};
-    System.out.println(RecursiveBinarySearch(arr, 0, arr.length - 1, -500));
+    System.out.println(RecursiveBinarySearch(arr, 0, arr.length - 1, 10));
   }
 
   private static int RecursiveBinarySearch(int[] arr, int low, int high, int item) {
@@ -16,9 +16,8 @@ public class a2_RecursiveBinarySearch {
       return mid;
     } else if (arr[mid] < item) {
       return RecursiveBinarySearch(arr, mid + 1, high, item);
-    } else if (arr[mid] > item) {
+    } else {
       return RecursiveBinarySearch(arr, low, mid - 1, item);
     }
-    return -1;
   }
 }
