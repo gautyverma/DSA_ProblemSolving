ff56be0f2a2ae7ed5433a7b7940bbd28c15a6e06
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-10 19:58:09 +0530

    [DSA][sorting][course]- Back to Work

---
+++ src/gfg/a0_Important/a0_Sorting_MergeOverlappingIntervals.java
@@ -0,0 +1,47 @@
+package gfg.a0_Important;
+
+import java.util.Arrays;
+
+class Interval implements Comparable<Interval> {
+
+  int s, e;
+
+  public Interval(int s, int e) {
+    this.s = s;
+    this.e = e;
+  }
+
+  @Override
+  public int compareTo(Interval object) {
+    return this.s - object.s;
+  }
+}
+
+public class a0_Sorting_MergeOverlappingIntervals {
+  public static void main(String[] args) {
+    Interval[] arr = {
+      new Interval(5, 10),
+      new Interval(2, 3),
+      new Interval(6, 8),
+      new Interval(1, 7),
+      new Interval(18, 30)
+    };
+    mergeIntervals(arr);
+  }
+
+  private static void mergeIntervals(Interval[] arr) {
+
+    Arrays.sort(arr);
+    int n = arr.length, res = 0;
+    for (int i = 1; i < n; i++) {
+      if (arr[res].e >= arr[i].s) {
+        arr[res].e = Math.max(arr[res].e, arr[i].e);
+        arr[res].s = Math.min(arr[res].s, arr[i].s);
+      } else {
+        res++;
+        arr[res] = arr[i];
+      }
+    }
+    for (int i = 0; i <= res; i++) System.out.print("[" + arr[i].s + ", " + arr[i].e + "] ");
+  }
+}
--- src/prac/AliceBobCricket.java
+++ src/prac/AliceBobCricket.java
@@ -3,47 +3,47 @@ package prac;
 import java.util.Arrays;
 
 public class AliceBobCricket {
-	
-	static int mod = (int)1e9+7;
-	
-	public static void main(String[] args) {
-		int n1 = 2; // alice
-		int n2 = 4; // bob
 
-		int alice[] = { 1, 3, 5 };
-		int bob[] = { 2, 4, 6 };
+    static int mod = (int) 1e9 + 7;
 
-		int prod1 = countways(alice, alice.length, n1);
-		int prod2 = countways(bob, bob.length, n2);
-		System.out.println(modProd(prod1,prod2));
+    public static void main(String[] args) {
+        int n1 = 2; // alice
+        int n2 = 4; // bob
 
-	}
+        int alice[] = {1, 3, 5};
+        int bob[] = {2, 4, 6};
 
-	private static int modProd(int a, int b) {
-		// TODO Auto-generated method stub
-		return(a%mod * b%mod)%mod;
-		
-	}
+        int prod1 = countways(alice, alice.length, n1);
+        int prod2 = countways(bob, bob.length, n2);
+        System.out.println(modProd(prod1, prod2));
 
-	private static int countways(int[] score, int m, int runs) {
-		// TODO Auto-generated method stub
-		int[] table = new int[runs + 1];
-		Arrays.fill(table, 0);
+    }
 
-		table[0] = 1;
+    private static int modProd(int a, int b) {
+        // TODO Auto-generated method stub
+        return (a % mod * b % mod) % mod;
 
-		for (int i = 0; i < m; i++) {
-			for(int j = score[i];j<= runs;j++) {
-				table[j] = modSum(table[j],table[j-score[i]]);
-			}
-		}
+    }
 
-		return table[runs];
-	}
+    private static int countways(int[] score, int m, int runs) {
+        // TODO Auto-generated method stub
+        int[] table = new int[runs + 1];
+        Arrays.fill(table, 0);
 
-	private static int modSum(int a,int b) {
-		// TODO Auto-generated method stub
-		return(a%mod + b%mod)%mod;
-		
-	}
+        table[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = score[i]; j <= runs; j++) {
+                table[j] = modSum(table[j], table[j - score[i]]);
+            }
+        }
+
+        return table[runs];
+    }
+
+    private static int modSum(int a, int b) {
+        // TODO Auto-generated method stub
+        return (a % mod + b % mod) % mod;
+
+    }
 }
