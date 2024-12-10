a0034abd523ef7d7cbfa737454bfa940a818043e
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-10 19:58:47 +0530

    [DSA][linkedList][course]- a1_SimpleLLImpl

---
+++ src/gfg/a10_LinkedList/course/Node.java
@@ -0,0 +1,11 @@
+package gfg.a10_LinkedList.course;
+
+public class Node {
+  int data;
+  Node next;
+
+  Node(int value) {
+    data = value;
+    next = null;
+  }
+}
---
+++ src/gfg/a10_LinkedList/course/a1_SimpleLLImpl.java
@@ -0,0 +1,35 @@
+package gfg.a10_LinkedList.course;
+
+public class a1_SimpleLLImpl {
+  public static void main(String[] args) {
+
+/*
+// --- Basic Impl - 1
+
+    Node head = new Node(10);
+    Node t1 = new Node(15);
+    Node t2 = new Node(20);
+
+    head.next = t1;
+    t1.next = t2;
+*/
+
+//  --- Basic Impl - 2
+    Node head = new Node(10);
+    head.next = new Node(15);
+    head.next.next = new Node(20);
+    head.next.next.next = new Node(25);
+    head.next.next.next.next = new Node(30);
+    head.next.next.next.next.next = new Node(35);
+
+    printLL(head);
+  }
+
+  private static void printLL(Node head) {
+    Node curr = head;
+    while(curr != null) {
+      System.out.print(curr.data + " ");
+      curr = curr.next;
+    }
+  }
+}
