4a39493fde91bfa2356ce8b0e0af18c69eba82ef
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-11 00:09:23 +0530

    [DSA][linkedList][course]- a1_SimpleLLImpl - ops - Insertion at given position middle

--- src/gfg/a10_LinkedList/course/a1_SimpleLLImpl.java
+++ src/gfg/a10_LinkedList/course/a1_SimpleLLImpl.java
@@ -4,12 +4,10 @@ public class a1_SimpleLLImpl {
   public static void main(String[] args) {
 
     /*
-    // --- Basic Impl - 1
-
+        // --- Basic Impl - 1
         Node head = new Node(10);
         Node t1 = new Node(15);
         Node t2 = new Node(20);
-
         head.next = t1;
         t1.next = t2;
     */
@@ -22,8 +20,8 @@ public class a1_SimpleLLImpl {
         head.next.next.next = new Node(25);
         head.next.next.next.next = new Node(30);
         head.next.next.next.next.next = new Node(35);
-
     */
+
     Node head = null;
     head = insertAtEnd(head, 2);
     head = insertAtStart(head, 10);
@@ -46,6 +44,26 @@ public class a1_SimpleLLImpl {
     head = insertAtEnd(head, 13);
     printLL(head);
     System.out.println("\n-------------");
+
+    // ops - Insert at middle of LL after some passes
+    System.out.println("<-insertion at middle->");
+    head = insertAtMiddle(head, 3, 7);
+    printLL(head);
+    System.out.println("\n-------------");
+  }
+
+  private static Node insertAtMiddle(Node head, int pos, int value) {
+    Node temp = new Node(value);
+    if (pos == 1) {
+      temp.next = head;
+      return temp;
+    }
+    Node curr = head;
+    for (int i = 1; i <= pos - 2 && curr != null; i++) curr = curr.next;
+    if (curr == null) return head;
+    temp.next = curr.next;
+    curr.next = temp;
+    return head;
   }
 
   private static Node insertAtEnd(Node head, int value) {
