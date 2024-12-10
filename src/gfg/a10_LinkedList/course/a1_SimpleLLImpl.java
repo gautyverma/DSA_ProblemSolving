08bed25bf7a4cc9b0b2c6a2aa2ff2886a5e12896
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-10 22:06:42 +0530

    [DSA][linkedList][course]- a1_SimpleLLImpl - ops - Insertion at beginning

--- src/gfg/a10_LinkedList/course/a1_SimpleLLImpl.java
+++ src/gfg/a10_LinkedList/course/a1_SimpleLLImpl.java
@@ -3,18 +3,18 @@ package gfg.a10_LinkedList.course;
 public class a1_SimpleLLImpl {
   public static void main(String[] args) {
 
-/*
-// --- Basic Impl - 1
+    /*
+    // --- Basic Impl - 1
 
-    Node head = new Node(10);
-    Node t1 = new Node(15);
-    Node t2 = new Node(20);
+        Node head = new Node(10);
+        Node t1 = new Node(15);
+        Node t2 = new Node(20);
 
-    head.next = t1;
-    t1.next = t2;
-*/
+        head.next = t1;
+        t1.next = t2;
+    */
 
-//  --- Basic Impl - 2
+    //  --- Basic Impl - 2
     Node head = new Node(10);
     head.next = new Node(15);
     head.next.next = new Node(20);
@@ -22,17 +22,38 @@ public class a1_SimpleLLImpl {
     head.next.next.next.next = new Node(30);
     head.next.next.next.next.next = new Node(35);
 
-    // Traversing using loops
-    printLL(head);
-
     // Traversing using recursion
-    System.out.println(" \nRecursion ");
+    System.out.println("Recursion");
     printLLRecursive(head);
+    System.out.println("\n-------------");
+
+    // ops - Insert at beginning of LL
+    System.out.println("<-insertion at beginning- >");
+    head = insertAtStart(head, 7);
+    printLL(head);
+    System.out.println("\n-------------");
+  }
+
+  private static Node insertAtStart(Node head, int value) {
+    /*
+    if (head == null) {
+      head = new Node(value);
+    } else {
+      Node curr = head;
+      head = new Node(value);
+      head.next = curr;
+    }
+    return head;
+    */
+
+    Node temp = new Node(value);
+    temp.next = head;
+    return temp;
   }
 
   private static void printLLRecursive(Node head) {
-    if(head != null){
-      System.out.print(head.data +" ");
+    if (head != null) {
+      System.out.print(head.data + " ");
       printLLRecursive(head.next);
     }
     return;
@@ -40,7 +61,7 @@ public class a1_SimpleLLImpl {
 
   private static void printLL(Node head) {
     Node curr = head;
-    while(curr != null) {
+    while (curr != null) {
       System.out.print(curr.data + " ");
       curr = curr.next;
     }
