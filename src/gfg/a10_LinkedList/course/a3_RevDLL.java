085309e015c11cf50c0e62de03a39e1b157a2526
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-13 01:43:56 +0530

    [DSA][linkedList][course]- a3_RevDLL - Reversal of DLL

---
+++ src/gfg/a10_LinkedList/course/a3_RevDLL.java
@@ -0,0 +1,62 @@
+package gfg.a10_LinkedList.course;
+
+public class a3_RevDLL {
+  public static void main(String[] args) {
+    NodeDoubleLL head = new NodeDoubleLL(10);
+    NodeDoubleLL node1 = new NodeDoubleLL(20);
+    NodeDoubleLL node2 = new NodeDoubleLL(30);
+    NodeDoubleLL node3 = new NodeDoubleLL(40);
+
+    head.next = node1;
+    node1.prev = head;
+    node1.next = node2;
+    node2.prev = node1;
+    node2.next = node3;
+    node3.prev = node2;
+
+    printDLL(head);
+    System.out.println("----------------");
+
+    // ops - Reverse
+    System.out.println("<-Reverse->");
+    head = reverseDLL(head);
+    printDLL(head);
+    System.out.println("----------------");
+  }
+
+  private static NodeDoubleLL reverseDLL(NodeDoubleLL head) {
+    /*
+    if (head == null) return null;
+    NodeDoubleLL curr = head;
+    while (curr.next != null) curr = curr.next;
+
+    head = curr;
+
+    while (head.prev != null) {
+      System.out.print(head.data + " ");
+      head = head.prev;
+    }
+    System.out.println(head.data);
+    return null;
+    */
+    if(head == null || head.next == null)
+      return head;
+    NodeDoubleLL prev = null;
+    NodeDoubleLL curr = head;
+    while(curr != null){
+      prev = curr.prev;
+      curr.prev = curr.next;
+      curr.next = prev;
+      curr = curr.prev;
+    }
+    return prev.prev;
+  }
+
+  private static void printDLL(NodeDoubleLL head) {
+    while (head != null) {
+      System.out.print(head.data + " ");
+      head = head.next;
+    }
+    System.out.println();
+  }
+}
