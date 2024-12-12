6553380b5514833697513f77e44950340c9069ae
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-13 01:10:01 +0530

    [DSA][linkedList][course]- a2_DoubleLLImpl - Insert at start and end

--- src/gfg/a10_LinkedList/course/a2_DoubleLLImpl.java
+++ src/gfg/a10_LinkedList/course/a2_DoubleLLImpl.java
@@ -9,16 +9,56 @@ public class a2_DoubleLLImpl {
     NodeDoubleLL node3 = new NodeDoubleLL(40);
 
     head.next = node1;
-
     node1.prev = head;
     node1.next = node2;
-
     node2.prev = node1;
     node2.next = node3;
-
     node3.prev = node2;
 
     printDLL(head);
+    System.out.println("----------------");
+
+    // ops - Insert at beginning of DLL
+    System.out.println("<-insertion at beginning->");
+    head = insertAtStart(head, 5);
+    printDLL(head);
+    System.out.println("----------------");
+
+    // ops - Insert at ending of DLL
+    System.out.println("<-insertion at ending->");
+    head = insertAtEnd(head, 45);
+    printDLL(head);
+    System.out.println("----------------");
+  }
+
+  private static NodeDoubleLL insertAtEnd(NodeDoubleLL head, int value) {
+    NodeDoubleLL temp = new NodeDoubleLL(value);
+    if (head == null) return temp;
+    NodeDoubleLL curr = head;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    curr.next = temp;
+    temp.prev = curr;
+    return head;
+  }
+
+  private static NodeDoubleLL insertAtStart(NodeDoubleLL head, int value) {
+
+    NodeDoubleLL temp = new NodeDoubleLL(value);
+    /*
+
+        if (head == null) return temp;
+        else{
+          head.prev = temp;
+           temp.next = head;
+          head = temp;
+        }
+        return head;
+    */
+    temp.next = head;
+    if (head != null) head.prev = temp;
+    return temp;
   }
 
   private static void printDLL(NodeDoubleLL head) {
