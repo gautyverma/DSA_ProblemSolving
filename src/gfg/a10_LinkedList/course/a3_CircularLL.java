386cfe68184fe38e1c5cbf7d39c60b71a47b46aa
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-14 21:17:39 +0530

    [DSA][linkedList][course]- a3_CircularLL -ops- Delete head and kth position

--- src/gfg/a10_LinkedList/course/a3_CircularLL.java
+++ src/gfg/a10_LinkedList/course/a3_CircularLL.java
@@ -26,6 +26,55 @@ public class a3_CircularLL {
     head = insertAtEndEff(head, 45);
     printCLL(head);
     System.out.println("-------------");
+
+    // OPS - Deletion at start
+    System.out.println("Delete at Start of circular LL");
+    head = deleteAtStartNaive(head);
+    head = deleteAtStartEff(head);
+    printCLL(head);
+    System.out.println("-------------");
+
+    // OPS - Deletion at Kth position k<= number of nodes
+    System.out.println("Delete at Kth pos of circular LL");
+    head = deleteAtPos(head, 3);
+    printCLL(head);
+    head = deleteAtPos(head, 2);
+    printCLL(head);
+    System.out.println("-------------");
+  }
+
+  private static Node deleteAtPos(Node head, int pos) {
+    if (pos == 1) return deleteAtStartEff(head);
+    Node curr = head;
+    //    pos--; // To take curr as prev node of position node
+    while (pos != 2) {
+      pos--;
+      curr = curr.next;
+    }
+    /* -- other impl of above loop
+
+    for(int i = 0 ;i<pos-2;i++){
+          curr = curr.next;
+        }
+        */
+    curr.next = curr.next.next;
+    return head;
+  }
+
+  private static Node deleteAtStartEff(Node head) {
+    if (head == null || head.next == head) return null;
+
+    head.data = head.next.data;
+    head.next = head.next.next;
+    return head;
+  }
+
+  private static Node deleteAtStartNaive(Node head) {
+    if (head == null || head.next == head) return null;
+    Node curr = head;
+    while (curr.next != head) curr = curr.next;
+    curr.next = head.next;
+    return curr.next;
   }
 
   private static Node insertAtEndEff(Node head, int value) {
@@ -42,12 +91,13 @@ public class a3_CircularLL {
       return temp;
     }
   }
+
   private static Node insertAtEndNaive(Node head, int value) {
     Node temp = new Node(value);
     if (head == null) {
       temp.next = temp;
       return temp;
-    }else{
+    } else {
       Node curr = head;
       while (curr.next != head) curr = curr.next;
       curr.next = temp;
