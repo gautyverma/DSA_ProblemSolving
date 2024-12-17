64ee4b2d546df4d4d48851de2af6a5e798eb2b19
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-18 01:43:08 +0530

    [DSA][linkedList][course]- a7_ReverseLL

---
+++ src/gfg/a10_LinkedList/course/a7_ReverseLL.java
@@ -0,0 +1,23 @@
+package gfg.a10_LinkedList.course;
+
+public class a7_ReverseLL {
+  public static void main(String[] args) {
+    OPS_LL ll = new OPS_LL();
+    Node head = ll.initializeSortedLL();
+    ll.printLL(head);
+    head = reverseLL(head);
+    ll.printLL(head);
+  }
+
+  private static Node reverseLL(Node head) {
+    Node curr = head;
+    Node prev = null;
+    while (curr != null) {
+      Node next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev; // prev is new head
+  }
+}
