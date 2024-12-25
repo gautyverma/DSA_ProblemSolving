b7bf256820d8b3996e99588fd2e122d08324c89f
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-26 01:11:47 +0530

    [DSA][linkedList][course]- a11_DetectLoopLL - Floyd Algo

--- src/gfg/a10_LinkedList/course/a11_DetectLoopLL.java
+++ src/gfg/a10_LinkedList/course/a11_DetectLoopLL.java
@@ -5,12 +5,39 @@ import java.util.HashSet;
 public class a11_DetectLoopLL {
   public static void main(String[] args) {
     OPS_LL ll = new OPS_LL();
+
+    System.out.println(" ---- pointing dummy node ---- ");
     System.out.println(isLoopInLL(ll.initializeCycleLL()));
     System.out.println(isLoopInLL(ll.initializeCircularLL()));
 
+    System.out.println(" ---- Using hashSet ---- ");
     System.out.println(isLoopHashInLL(ll.initializeCycleLL()));
     System.out.println(isLoopHashInLL(ll.initializeCircularLL()));
+
+    System.out.println(" ---- Floyd cycle detection ---- ");
+    System.out.println(isLoopFloydInLL(ll.initializeCycleLL()));
+    System.out.println(isLoopFloydInLL(ll.initializeCircularLL()));
   }
+
+  private static boolean isLoopFloydInLL(Node head) {
+    Node slowP = head,fastP = head;
+    while(fastP != null && fastP.next !=null){
+      slowP = slowP.next;
+      fastP = fastP.next.next;
+
+      if(slowP == fastP)
+        return true;
+    }
+    return false;
+  }
+
+  /*
+  * Detect loop using floyd cycle detection
+  * 1. Initialize slowPointer and fastPointer from head.
+  * 2. Move slowPointer by 1 and fastPointer by 2. If they meet loop is there.
+  * */
+
+
 
   /*
    * Traverse LL, if node visited add to hashSet and if again visited check if present already
