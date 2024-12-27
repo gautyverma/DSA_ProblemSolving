35a4e29d1061253f7529a7ec2f2bfa7044a4dfb2
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-12-28 02:07:04 +0530

    [DSA][linkedList][course]- a15_MergeTwoSortedLL

---
+++ src/gfg/a10_LinkedList/course/a15_MergeTwoSortedLL.java
@@ -0,0 +1,39 @@
+package gfg.a10_LinkedList.course;
+
+public class a15_MergeTwoSortedLL {
+  public static void main(String[] args) {
+    OPS_LL ll = new OPS_LL();
+    Node head1 = ll.initializeSortedLL();
+    Node head2 = ll.initializeSortedLL();
+    ll.printLL(mergeSortedLL(head1, head2));
+  }
+
+  private static Node mergeSortedLL(Node head1, Node head2) {
+    if (head1 == null) return head2;
+    if (head2 == null) return head1;
+    Node head = null, tail = null;
+    if (head1.data <= head2.data) {
+      head = head1;
+      tail = head1;
+      head1 = head1.next;
+    } else {
+      head = head2;
+      tail = head2;
+      head2 = head2.next;
+    }
+    while (head1 != null && head2 != null) {
+      if (head1.data <= head2.data) {
+        tail.next = head1;
+        tail = head1;
+        head1 = head1.next;
+      } else {
+        tail.next = head2;
+        tail = head2;
+        head2 = head2.next;
+      }
+    }
+    if (head1 == null) tail.next = head2;
+    else tail.next = head1;
+    return head;
+  }
+}
