c41878b32cb58f0e9a9a79ce8d5b3d689b9659de
GAUTY <gautyverma@gmail.com>
2025-04-19 01:59:08 +0530

    [DSA][Tree][course] - a1_BinaryTreeTraverse - pre/post traversal

--- src/gfg/a14_Tree/common/OPS_Tree.java
+++ src/gfg/a14_Tree/common/OPS_Tree.java
@@ -6,6 +6,23 @@ import java.util.Queue;
 public class OPS_Tree {
 
   public Node initializeTree() {
+    Node root = new Node(10);
+    // left subtree
+    root.left = new Node(20);
+    root.left.left = new Node(40);
+    root.left.right = new Node(50);
+
+    root.left.right.left = new Node(70);
+    root.left.right.right = new Node(80);
+
+    // right subtree
+    root.right = new Node(30);
+    root.right.right = new Node(60);
+    return root;
+  }
+
+
+  public Node initializeTreeUsingInsertion() {
     Node root = insertNode(null, 10);
     root = insertNode(root, 20);
     root = insertNode(root, 30);
@@ -55,6 +72,22 @@ public class OPS_Tree {
       inorderTraversal(root.left);
       System.out.print(root.key + " ");
       inorderTraversal(root.right);
+    }
+  }
+
+  public void preorderTraversal(Node root) {
+    if (root != null) {
+      System.out.print(root.key + " ");
+      preorderTraversal(root.left);
+      preorderTraversal(root.right);
+    }
+  }
+
+  public void postorderTraversal(Node root) {
+    if (root != null) {
+      postorderTraversal(root.left);
+      postorderTraversal(root.right);
+      System.out.print(root.key + " ");
     }
   }
 }
--- src/gfg/a14_Tree/course/a1_BinaryTreeTraverse.java
+++ src/gfg/a14_Tree/course/a1_BinaryTreeTraverse.java
@@ -4,9 +4,14 @@ import gfg.a14_Tree.common.Node;
 import gfg.a14_Tree.common.OPS_Tree;
 
 public class a1_BinaryTreeTraverse {
-    public static void main(String[] args){
-        OPS_Tree tree = new OPS_Tree();
-        Node root = tree.initializeTree();
-        tree.inorderTraversal(root);
-    }
+  public static void main(String[] args) {
+    OPS_Tree tree = new OPS_Tree();
+    Node root = tree.initializeTree();
+    System.out.println("\n --- Inorder --- ");
+    tree.inorderTraversal(root);
+    System.out.println("\n --- preorder --- ");
+    tree.preorderTraversal(root);
+    System.out.println("\n --- postorder --- ");
+    tree.postorderTraversal(root);
+  }
 }
