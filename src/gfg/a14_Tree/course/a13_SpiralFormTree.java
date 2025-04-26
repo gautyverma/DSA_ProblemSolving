72c77d612a26db7590a7399fca12de4572da3897
GAUTY <gautyverma@gmail.com>
2025-04-26 15:25:13 +0530

    [DSA][Tree][course] - a13_SpiralFormTree - method - 2 - using two operation

--- src/gfg/a14_Tree/course/a13_SpiralFormTree.java
+++ src/gfg/a14_Tree/course/a13_SpiralFormTree.java
@@ -11,10 +11,48 @@ public class a13_SpiralFormTree {
   public static void main(String[] args) {
     OPS_Tree tree = new OPS_Tree();
     Node root = tree.initializeTree();
-    printSpiralForm(root);
+    printSpiralFormApp1(root);
+    System.out.println("\nTwo stack approach : ");
+    printSpiralFormApp2(root);
   }
 
-  private static void printSpiralForm(Node root) {
+  /*
+   * Using two stack
+   *   1. push root to stack s1
+   *   2. While any of two stacks are empty
+   *       ==> while s1 is not empty
+   *         a) take out a node print it.
+   *         b) push taken out node children to s2
+   *
+   *       ==> while s2 is not empty
+   *         a) take out a node print it.
+   *         b) push children of taken out node into s1 in reverse order
+   * */
+  private static void printSpiralFormApp2(Node root) {
+    if (root == null) return;
+    Stack<Node> s1 = new Stack<>();
+    Stack<Node> s2 = new Stack<>();
+
+    s1.push(root);
+    while (!s1.isEmpty() || !s2.isEmpty()) {
+      while (!s1.isEmpty()) {
+        Node curr = s1.pop();
+        System.out.print(curr.key + " ");
+
+        if (curr.left != null) s2.push(curr.left);
+        if (curr.right != null) s2.push(curr.right);
+      }
+      while (!s2.isEmpty()) {
+        Node curr = s2.pop();
+        System.out.print(curr.key + " ");
+
+        if (curr.right != null) s1.push(curr.right);
+        if (curr.left != null) s1.push(curr.left);
+      }
+    }
+  }
+
+  private static void printSpiralFormApp1(Node root) {
     if (root == null) return;
     Queue<Node> q = new LinkedList<>();
     q.add(root);
@@ -37,7 +75,7 @@ public class a13_SpiralFormTree {
         }
       }
       reverse = !reverse;
-      System.out.println();
+      //      System.out.println();
     }
   }
 }
