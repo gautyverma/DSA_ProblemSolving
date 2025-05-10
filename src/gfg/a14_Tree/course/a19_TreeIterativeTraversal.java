64633458b90a9476871d869f7ba54c06137ee207
GAUTY <gautyverma@gmail.com>
2025-05-10 16:21:30 +0530

    [DSA][Tree][course] - a19_TreeIterativeTraversal - preorder iterative traversal

--- src/gfg/a14_Tree/course/a19_TreeIterativeTraversal.java
+++ src/gfg/a14_Tree/course/a19_TreeIterativeTraversal.java
@@ -9,22 +9,43 @@ public class a19_TreeIterativeTraversal {
   public static void main(String[] args) {
     OPS_Tree tree = new OPS_Tree();
     Node root = tree.initializeTree();
+    System.out.println("\nrecursive inorder traversal");
     tree.inorderTraversal(root);
-    System.out.println("\nInorder tree traversal:");
+    System.out.println("\niterative Inorder tree traversal:");
     inorderIterTraversal(root);
+    System.out.println("\n----------------");
+    System.out.println("\nrecursive preorder traversal");
+    tree.preorderTraversal(root);
+    System.out.println("\niterative preorder tree traversal:");
+    preorderIterTraversal(root);
+    System.out.println("\niterative preorder tree traversal: space optimize");
+    preorderIterTraversalEff(root);
   }
 
-  private static void inorderIterTraversal(Node root) {
+  private static void inorderIterTraversal(Node root) {}
+
+  private static void preorderIterTraversal(Node root) {
     Stack<Node> st = new Stack<>();
+    st.push(root);
+    while (!st.isEmpty()) {
+      Node curr = st.pop();
+      System.out.print(curr.key + " ");
+      if (curr.right != null) st.push(curr.right);
+      if (curr.left != null) st.push(curr.left);
+    }
+  }
+
+  private static void preorderIterTraversalEff(Node root) {
+    Stack<Node> st = new Stack<>();
+    st.push(root);
     Node curr = root;
-    while (curr != null || !st.isEmpty()) {
+    while (!st.isEmpty()) {
       while (curr != null) {
-        st.push(curr);
+        System.out.print(curr.key + " ");
+        if (curr.right != null) st.push(curr.right);
         curr = curr.left;
       }
       curr = st.pop();
-      System.out.print(curr.key + " ");
-      curr = curr.right;
     }
   }
 }
