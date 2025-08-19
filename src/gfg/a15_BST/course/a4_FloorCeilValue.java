ff2d4aa69cd421933b75a92041a84bf4bc957e56
GAUTY <gautyverma@gmail.com>
2025-08-19 22:14:39 +0530

    [DSA][BST][course] - a4_FloorCeilValue

--- src/gfg/a15_BST/course/a4_FloorCeilValue.java
+++ src/gfg/a15_BST/course/a4_FloorCeilValue.java
@@ -12,12 +12,44 @@ import gfg.a15_BST.common.OPS_BST;
           / \
         70   90
 * */
-public class a4_FloorValue {
+
+/*
+* Given a Binary Search Tree and a number x, the task is to find the floor of x in the given BST,
+* where floor means the greatest value node of the BST which is smaller than or equal to x. if x is smaller than the smallest node of BST then return -1.
+* */
+public class a4_FloorCeilValue {
   public static void main(String[] args) {
     OPS_BST bst = new OPS_BST();
     Node root = bst.initializeBST();
     System.out.println("FloorValue: using recursion - " + floorRecursive(root, 85));
     System.out.println("FloorValue: using iter approach - " + floorIter(root, 85));
+    System.out.println("CeilValue: using recursion - " + ceilRecursive(root, 85));
+    System.out.println("CeilValue: using iter - " + ceilIter(root, 85));
+  }
+
+  private static int ceilIter(Node root, int key) {
+    int ceilValue = -1;
+    while (root != null) {
+      if (root.data == key) return root.data;
+      if (root.data < key) root = root.right;
+      else {
+        ceilValue = root.data;
+        root = root.left;
+      }
+    }
+    return ceilValue;
+  }
+
+
+  private static int ceilRecursive(Node root, int key) {
+    if(root == null)
+      return -1;
+    if(root.data == key)
+      return root.data;
+    if(root.data < key)
+      return ceilRecursive(root.right,key);
+    int ceilValue = ceilRecursive(root.left,key);
+    return (ceilValue >= key && ceilValue != -1) ? ceilValue : root.data;
   }
 
   private static int floorIter(Node root, int key) {
