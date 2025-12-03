a7c9815e2098c3dc8cbba2bcaa6647d8dbd0ea63
GAUTY <gautyverma@gmail.com>
2025-12-03 08:16:26 +0530

    [DSA][Heap][course] - a1_KmostOccuringElements

--- src/gfg/a16_Heap/exercise/a1_KmostOccuringElements.java
+++ src/gfg/a16_Heap/exercise/a1_KmostOccuringElements.java
@@ -3,6 +3,25 @@ package gfg.a16_Heap.exercise;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
+/*
+* Given an array arr[] in which elements may be repeating several times.
+* Also, a positive number k is given and the task is to find sum of total frequencies of k most occurring elements.
+
+Note: The value of k is guaranteed to be less than or equal to the number of distinct elements in arr.
+
+Examples :
+
+Input: arr[] = [3, 1, 4, 4, 5, 2, 6, 1], k = 2
+Output: 4
+
+* Explanation: Here, 4 and 1 are 2 most occurring elements in the array,
+* both appearing twice. So, total sum of thier frequencies is 2 + 2 = 4.
+Input: arr[] = [3, 3, 3, 4, 1, 1, 6, 1], k = 2
+Output: 6
+Explanation: Here, 3 and 1 are 2 most frequent elements in the array,
+* both appearing three times. Hence, the total sum of their frequencies is 3 + 3 = 6.
+* */
+
 public class a1_KmostOccuringElements {
   public static void main(String[] args) {
     int[] arr = {3, 1, 4, 4, 5, 2, 6, 1};
@@ -32,5 +51,5 @@ public class a1_KmostOccuringElements {
     }
 
     return sum;
-    }
+  }
 }
